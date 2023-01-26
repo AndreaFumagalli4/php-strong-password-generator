c79cff52c8f8c922b56d4edac47680f80f1a5242
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-01-26 14:41:25 +0100

    create form

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Stronger Password Generator</title>
+    </head>
+    <body>
+        <header>
+            <h1>
+                Password generator
+            </h1>
+        </header>
+        <main>
+            <form action="./index.php" method="GET">
+                <label for="characters">
+                    Inserisci il numero di caratteri che avrà la password
+                </label>
+                <input type="text" name="characters">
+                <button type="submit">Invia</button>
+            </form>
+        </main>
+    </body>
+</html>
